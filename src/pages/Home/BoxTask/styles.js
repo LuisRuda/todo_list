1aaa92ba54a98f08b5ec3c76854807aafc900ba0
Luis Paulo Ruda <luispauloruda@gmail.com>
2020-06-27 19:05:05 -0300

    Function to select/deselect task

--- src/pages/Home/BoxTask/styles.js
+++ src/pages/Home/BoxTask/styles.js
@@ -1,20 +1,28 @@
-import styled, { css } from 'styled-components/native';
+import styled from 'styled-components/native';
 import { Dimensions } from 'react-native';
+
+import colors from '../../../assets/colors';
 
 export const Container = styled.View`
   width: ${Dimensions.get('window').width - 60}px;
+  flex-direction: row;
   background-color: #e5e5e5;
   align-self: center;
+  align-items: center;
   border-radius: 10px;
-  padding: 20px;
-  margin-top: 12px;
+  padding: 10px 15px;s
+  margin-bottom: 12px;
   elevation: 1;
+`;
 
-  ${(props) =>
-    props.first &&
-    css`
-      margin-top: 0;
-    `};
+export const CheckBox = styled.TouchableOpacity`
+  height: 26px;
+  width: 26px;
+  border-radius: 4px;
+  margin-right: 12px;
+  align-items: center;
+  justify-content: center;
+  background-color: ${colors.white};
 `;
 
 export const Text = styled.Text``;
